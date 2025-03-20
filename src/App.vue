2cea429fea37859a4d1e548d12009b03a56a5923
JPWake <jperlin@my.waketech.edu>
2025-03-20 19:12:56 -0400

    Fixing Previous JS Errors Part 3

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,22 @@
 <template>
-  <div class="hello">
+  <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Random Fact:</h1>
-    <button @click="fetchData">Click Me!</button>
-    <p v-if="fact">{{ fact }}</p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      fact: "",
-    };
-  },
-  methods: {
-    fetchData() {
-      fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', {
-        method: "GET",
-        headers: {
-          'x-rapidapi-key': 'cb4545bcdamsh167c0cb6685f900p14e854jsn9f8035654a5b',
-          'x-rapidapi-host': 'facts-by-api-ninjas.p.rapidapi.com',
-        },
-      })
-        .then((response) => {
-          response.json().then((data) => {
-            this.fact = data[0].fact;
-          });
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    },
-  },
-};
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
-<style scoped>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -47,11 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-button {
-padding: 12px 32px;
-font-size: 16px;
-border-radius: 8px;
 }
 </style>
