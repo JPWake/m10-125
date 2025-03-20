c6f103f976478746ff5c9d5a5729e6b671208293
JPWake <jperlin@my.waketech.edu>
2025-03-20 19:24:46 -0400

    Fixing Previous JS Errors Part 4

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,54 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Random Fact:</h1>
+    <button @click="fetchData">Click Me!</button>
+    <p v-if="fact">{{ fact }}</p>
   </div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      fact: "",
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": 'cb4545bcdamsh167c0cb6685f900p14e854jsn9f8035654a5b',
+          "X-RapidAPI-Host": 'facts-by-api-ninjas.p.rapidapi.com',
+        },
+      })
+        .then((response) => {
+          response.json().then((data) => {
+            this.fact = data[0].fact;
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+};
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+button {
+	padding: 12px 32px;
+	font-size: 16px;
+	border-radius: 8px;
+}
+</style>
 
 <style>
 #app {
@@ -26,3 +61,7 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+
+
