364332eb68bdc6b58415e24fb072342e99299e0f
JPWake <jperlin@my.waketech.edu>
2025-03-20 19:26:37 -0400

    Fixing Previous JS Errors Part 5

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,8 @@ export default {
       fetch('https://facts-by-api-ninjas.p.rapidapi.com/v1/facts', {
         method: "GET",
         headers: {
-          "X-RapidAPI-Key": 'cb4545bcdamsh167c0cb6685f900p14e854jsn9f8035654a5b',
-          "X-RapidAPI-Host": 'facts-by-api-ninjas.p.rapidapi.com',
+          "x-rapidapi-key": 'cb4545bcdamsh167c0cb6685f900p14e854jsn9f8035654a5b',
+          "x-rapidapi-host": 'facts-by-api-ninjas.p.rapidapi.com',
         },
       })
         .then((response) => {
